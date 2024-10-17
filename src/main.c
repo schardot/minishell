42c9169c7513428a59666d0228235a60e613ce2c
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-10-17 09:14:35 +0200

    made some comments and added "add_history()"

--- src/main.c
+++ src/main.c
@@ -16,19 +16,17 @@ int main(int argc, char **argv)
 	}
 	while (1)
 	{
-		input = readline("\033[1;36mnatalia's shell\033[95m$ \033[0m");
-		// input = readline("\033[1;36mminishell\033[95m$ \033[0m");
+		input = readline("\033[1;36mminishell\033[95m$ \033[0m"); //this is an allowed external function that prints what we pass to it and acts line get_next_line(0)
 		len = ft_strlen(input);
-		if (len > 0 && input[len - 1] == '\n')
+		if (len > 0 && input[len - 1] == '\n') //we have to take out the newline
 			input[ft_strlen(input) - 1] = '\0';
-		// if (input)
-		// 	add_to_history(); ---------------- implement
+		add_history(input); //adds the input to the history, its also an allowed external function
 		if (ft_strncmp(input, "exit", len) == 0)
 		{
 			free (input);
 			break;
 		}
-		if (ft_strncmp(input, "ls", len) == 0)
+		if (ft_strncmp(input, "ls", len) == 0) //im just trying to implement ls and understand how execve works
 		{
 			pid = fork();
 			if (pid == 0)
