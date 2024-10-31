6dd65e6f302674e16bfad00780144615e3c7516c
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2024-10-31 18:25:39 +0000

    implemented the base of unset, function is actually doing nothing for now

--- src/builtins.c
+++ src/builtins.c
@@ -98,14 +98,23 @@ int	builtinexport(t_tools *t, t_scmd *node)
 	return (0);
 }
 
-int	builtinunset(t_tools *t, t_scmd *node)
+int builtinunset(t_tools *t, t_scmd *scmd)
 {
-	/* - - - - - - - - - - - - - - - - - - - - - CHAT GPT - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   **unset** (with no options):
-   - Removes environment variables.
-   - Example: `unset VAR` removes `VAR` from the environment.*/
-	(void)node;
-	(void)t;
+	if (scmd->argsc == 1)
+		return (0); // $unset does nothing
+	if (check_unset_args(scmd))
+	{
+		printf("minishell: unset: \'%s\': not a valid identifier\n", scmd->args[1]);
+		return (1);
+	}
+	return (0);
+}
+
+int check_unset_args(t_scmd *scmd)
+{
+	if (!ft_isalpha(scmd->args[1][0]))
+		return (1);
+	//there are more examples of whats an invalid argument, but for now i just want to build the idea of the thing.
 	return (0);
 }
 
