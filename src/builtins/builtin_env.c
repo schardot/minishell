5c5d6bac91de720313b9702580cd79995c52a2ef
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2024-11-06 17:49:10 +0000

    now allocating memory for  get_env

--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 10:50:19 by nataliascha       #+#    #+#             */
-/*   Updated: 2024/11/06 14:03:59 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/06 17:42:07 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,14 +37,14 @@ char	*ft_getenv(char *env, t_tools *t)
 	int	i;
 
 	i = 0;
-	env_len = strlen(env);
+    env_len = strlen(env);
 	while (t->envp[i])
 	{
-		if (ft_strncmp(env, t->envp[i], env_len) == 0)
+        if (ft_strncmp(env, t->envp[i], env_len) == 0)
 		{
-			if (t->envp[i][env_len] == '=')
-				return (t->envp[i]);
-		}
+            if (t->envp[i][env_len] == '=')
+                return (ft_strdup(t->envp[i]));
+        }
 		i++;
 	}
 	return (NULL);
