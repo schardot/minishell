496fe72e8f9654680473ee5bda8b476ce28150cd
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2024-11-06 15:07:55 +0000

    improving error handling

--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 10:50:19 by nataliascha       #+#    #+#             */
-/*   Updated: 2024/11/06 08:51:18 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/06 14:03:59 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,34 @@ int	builtinenv(t_tools *t, t_scmd *scmd)
 	int	i;
 
 	i = 0;
-	// if (scmd->args[1])
-	// {
-	//     printf("env: '%s': No such file or directory\n", scmd->args[1]);
-	//     return (EXIT_FAILURE);
-	// }
-	while(t->envp[i])
+	if (scmd->args[1])
+	{
+		ft_error(E_NO_SUCH_FILE, "env", scmd->args[1], t);
+		return (EXIT_FAILURE);
+	}
+	while (t->envp[i])
 	{
 		printf("%s\n", t->envp[i]);
 		i ++;
 	}
 	return (EXIT_SUCCESS);
+}
+
+char	*ft_getenv(char *env, t_tools *t)
+{
+	int	env_len;
+	int	i;
+
+	i = 0;
+	env_len = strlen(env);
+	while (t->envp[i])
+	{
+		if (ft_strncmp(env, t->envp[i], env_len) == 0)
+		{
+			if (t->envp[i][env_len] == '=')
+				return (t->envp[i]);
+		}
+		i++;
+	}
+	return (NULL);
 }
--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 10:50:30 by nataliascha       #+#    #+#             */
-/*   Updated: 2024/11/06 10:54:07 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/06 14:00:32 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	builtinunset(t_tools *t, t_scmd *scmd)
 {
 	if (check_unset_args(scmd, t))
 		return (EXIT_FAILURE);
-	if (!scmd->args[1])
-		export_empty();
 	else if (scmd->args[1])
 		create_new_envp(scmd, t);
 	return (EXIT_SUCCESS);
@@ -36,24 +34,14 @@ int	check_unset_args(t_scmd *scmd, t_tools *t)
 		while (scmd->args[j][i])
 		{
 			if (!ft_isalnum(scmd->args[j][i]) && !(scmd->args[j][i] == '_'))
-			{
-                ft_error(E_NOT_A_VALID_ID, scmd->args[0], scmd->args[j], t);
-                t->exit_status = 1;
-			}
+				ft_error(E_NOT_A_VALID_ID, scmd->args[0], scmd->args[j], t);
 			i++;
 		}
 		j ++;
 	}
-    if (t->exit_status == 1)
-        return (EXIT_FAILURE);
+	if (t->exit_status == 1)
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
-}
-
-void	export_empty(void)
-{
-	int	i;
-
-	i = 0;
 }
 
 int	create_new_envp(t_scmd *scmd, t_tools *t)
