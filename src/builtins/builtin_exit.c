4dc2e2172ec8d22f061019b7cfa09fb529e6abab
elmira <ekechedz@student.42berlin.de>
2024-12-03 16:43:34 +0100

    exit working

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nleite-s <nleite-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ekechedz <ekechedz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:10:11 by nleite-s          #+#    #+#             */
-/*   Updated: 2024/12/02 21:48:59 by nleite-s         ###   ########.fr       */
+/*   Updated: 2024/12/03 14:46:51 by ekechedz         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../include/parser.h"
@@ -22,38 +22,37 @@ int	builtinexit(t_tools *t, t_scmd *scmd)
 	if (scmd->args[1])
 	{
 		t->exit_status = check_exit_args(t, scmd);
-		if (t->exit_status != 0)
+		if (t->exit_status != 0 && scmd->argsc <= 2)
 			exit (t->exit_status);
 	}
-	if (scmd->pipecount == 0)
+	if (scmd->pipecount == 0 && scmd->argsc <= 2)
 	{
 		printf("exit\n");
 		t->exit = 1;
+		free_structs(t);
+		status = t->exit_status;
+		free_tools(t);
+		exit (status);
 	}
-	free_structs(t);
-	status = t->exit_status;
-	free_tools(t);
-	exit (status);
+	return (t->exit_status);
 }
 
 static int	check_exit_args(t_tools *t, t_scmd *scmd)
 {
-	int	exit_stat;
-
-	exit_stat = 0;
-	if (scmd->pipecount == 0)
-		printf("exit\n");
 	if (ft_checkstr(scmd->args[1], &ft_isdigit) != 0)
 	{
-		ft_error(E_NUM_ARG_REQUIRED, "exit", scmd->args[1], t);
+		if (ft_strncmp(scmd->args[1], "--", 2))
+			ft_error(E_NUM_ARG_REQUIRED, "exit", scmd->args[1], t);
 		return (t->exit_status);
 	}
 	if (scmd->argsc > 2)
 	{
+		printf("exit\n");
 		ft_error(E_TOO_MANY_ARGS, "exit", scmd->args[1], t);
-		return (t->exit_status);
+		t->exit_status = 1;
+
 	}
-	if (ft_checkstr(scmd->args[1], &ft_isdigit) == 0)
+	else if (ft_checkstr(scmd->args[1], &ft_isdigit) == 0)
 	{
 		t->exit_status = ft_atoi(scmd->args[1]);
 		if (t->exit_status > 255)
