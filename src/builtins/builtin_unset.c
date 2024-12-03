71238af5583606c356e4705d2c3631e7661ae61e
Natalia Schardosim <natalia.schardosim@hotmail.com>
2024-12-03 17:34:16 +0100

    fixed unset args and norminetted it

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   builtin_unset.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nleite-s <nleite-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nleite-s <nleite-s@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 10:50:30 by nataliascha       #+#    #+#             */
-/*   Updated: 2024/12/03 12:03:23 by nleite-s         ###   ########.fr       */
+/*   Created: 2024/12/03 17:33:53 by nleite-s          #+#    #+#             */
+/*   Updated: 2024/12/03 17:33:54 by nleite-s         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "../../include/parser.h"
 #include "../../include/minishell.h"
@@ -40,39 +40,9 @@ static int	check_unset_args(t_scmd *s, t_tools *t)
 
 	if (check_option(s, t) == 2)
 		return (t->exit_status);
-	check_argument(s, t);
 	if (t->exit_status == 1)
 		return (t->exit_status);
 	return (EXIT_SUCCESS);
-}
-
-static void	check_argument(t_scmd *s, t_tools *t)
-{
-	int	j;
-	int	i;
-
-	j = 1;
-	while (s->args[j])
-	{
-		if (s->args[j][0] == '\0')
-		{
-			ft_error(E_NOT_VALID_ID, s->args[0], s->args[j], t);
-			t->exit_status = 1;
-			j++;
-			continue ;
-		}
-		i = 0;
-		while (s->args[j][i])
-		{
-			if (!ft_isalnum(s->args[j][i]) && !(s->args[j][i] == '_'))
-			{
-				ft_error(E_NOT_VALID_ID, s->args[0], s->args[j], t);
-				t->exit_status = 1;
-			}
-			i++;
-		}
-		j++;
-	}
 }
 
 static int	check_option(t_scmd *s, t_tools *t)
